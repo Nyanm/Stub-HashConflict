5bbc5f3b6b8bcf87c6311dc3e26a6e9d0ee66d43
nyanm <nyanm2k@gmail.com>
2021-11-11 21:44:24 +0800

    Add dual process

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ small = 2 ** 32 - 1
 
 sample_s = 1000000
 sample_b = 10 * sample_s
+err_size = 10
 
 hashed_size = 2 ** 24
 shake = hashlib.shake_128()
@@ -62,7 +63,7 @@ def process(hash_method, get_data, size: int) -> np.array:
     for index in range(size):
         data_set.append(get_data(hash_method))
 
-    err_len = 15
+    err_len = err_size
     err = np.zeros(err_len, dtype=np.uint32)
     for data in data_set:
         if hashed[data]:
@@ -73,7 +74,34 @@ def process(hash_method, get_data, size: int) -> np.array:
     return err
 
 
-if __name__ == '__main__':
+def dual_process(hash_method_1, hash_method_2, size: int):
+    data_set_1, data_set_2 = [], []
+    hashed_1 = np.zeros(hashed_size, dtype=np.uint8)
+    hashed_2 = np.zeros(hashed_size, dtype=np.uint8)
+
+    for index in range(size):
+        raw = random.randint(0, big)
+        press_1_1 = hash_method_1(raw, 96)
+        press_1_2 = hash_method_1(press_1_1, 48)
+        data_set_1.append(press_1_2)
+        press_2_1 = hash_method_2(raw, 96)
+        press_2_2 = hash_method_2(press_2_1, 48)
+        data_set_2.append(press_2_2)
+
+    err = np.zeros((err_size, err_size), dtype=int)
+    for index in range(size):
+        if hashed_1[data_set_1[index]] and hashed_2[data_set_2[index]]:
+            err[hashed_1[data_set_1[index]] + 1][hashed_2[data_set_2[index]] + 1] += 1
+            err[hashed_1[data_set_1[index]]][hashed_2[data_set_2[index]]] -= 1
+        hashed_1[data_set_1[index]] += 1
+        hashed_2[data_set_2[index]] += 1
+
+    err = err[2:, 2:]
+
+    return err
+
+
+def conflict_test():
     uni_s = process(hash_method=xor_hash, get_data=get_uni, size=sample_s)
     sep_s = process(hash_method=xor_hash, get_data=get_sep, size=sample_s)
     uni_b = process(hash_method=xor_hash, get_data=get_uni, size=sample_b)
@@ -94,10 +122,14 @@ if __name__ == '__main__':
 
     print(msg, m_1, m_2, m_3, m_4, sep='\n')
 
-"""
-|Name     |Sample    |2 times  |3 times  |4 times  |5 times  |6 times  |7 times  |8 times  |9 times  |10 times
- UniSmall  1000000    27850     575       12        0         0         0         0         0         0        
- SepSmall  1000000    27869     557       12        0         0         0         0         0         0        
- UniBig    10000000   1642449   327159    48417     5753      592       56        7         0         0        
- SepBig    10000000   1643721   326907    48684     5890      560       48        4         0         0        
-"""
+    """
+    |Name     |Sample    |2 times  |3 times  |4 times  |5 times  |6 times  |7 times  |8 times  |9 times  |10 times
+     UniSmall  1000000    27850     575       12        0         0         0         0         0         0        
+     SepSmall  1000000    27869     557       12        0         0         0         0         0         0        
+     UniBig    10000000   1642449   327159    48417     5753      592       56        7         0         0        
+     SepBig    10000000   1643721   326907    48684     5890      560       48        4         0         0        
+    """
+
+
+if __name__ == '__main__':
+    print(dual_process(xor_hash, sum_hash, sample_b))
